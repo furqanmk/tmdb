6073ec48d49e528d71eeead2217f7cbc64a073bf
Furqan Muhammad Khan <furqanmk@outlook.com>
2017-05-17 18:29:08 +0500

    Project created

---
+++ The Movie DatabaseTests/The_Movie_DatabaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Movie_DatabaseTests.swift
+//  The Movie DatabaseTests
+//
+//  Created by Furqan on 17/05/2017.
+//  Copyright © 2017 Careem. All rights reserved.
+//
+
+import XCTest
+@testable import The_Movie_Database
+
+class The_Movie_DatabaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
