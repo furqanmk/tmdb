814237ab9d17294f5ff8941788a63adeb5019d57
Furqan Muhammad Khan <furqanmk@outlook.com>
2017-05-17 18:44:53 +0500

    Downloader implemented

---
+++ The Movie Database/DataConvertible.swift
@@ -0,0 +1,9 @@
+//
+//  DataConvertible.swift
+//  The Movie Database
+//
+//  Created by Furqan on 17/05/2017.
+//  Copyright © 2017 Careem. All rights reserved.
+//
+
+import Foundation
---
+++ The Movie Database/Logger.swift
@@ -0,0 +1,9 @@
+//
+//  Logger.swift
+//  The Movie Database
+//
+//  Created by Furqan on 17/05/2017.
+//  Copyright © 2017 Careem. All rights reserved.
+//
+
+import Foundation
